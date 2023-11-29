936afecd235d49790eeda3b5f5b2fcf281ee325a
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2023-11-29 18:26:37 +0530

    added total test case pass fail logging

--- src/ManualTesting.ts
+++ src/ManualTesting.ts
@@ -1,18 +1,28 @@
 import { Solution, Test, TestCase } from "./types";
 
+import _ from "lodash";
 import chalk from "chalk";
 import { log } from "console";
-import _ from "lodash";
 
 class ManualTesting implements Test {
 	test: (solution: Solution<any, any>) => void = solution => {
+		// get all the implementations and test-cases
 		const implementations = solution.getImplementations();
 		const testCases = solution.getTestCases();
 
-		log(`Running ${solution.getName()}`);
+		// let the client know test has started
+		console.log("\x1b[2J"); // clears the terminal
+		log(chalk.whiteBright(`\n\nRunning ${solution.getName()}`));
+
+		// measure all test-case runs
+		const totalTestCaseRuns = testCases.length * implementations.length;
+		let successfulRuns = 0;
 
 		for (const implementation of implementations) {
+			let successfulRunsOfCurrImplementation = 0;
+
 			for (const testCase of testCases) {
+				// log current solution name with input
 				log(
 					`\n${chalk.gray("Solution")} ${chalk.blue(
 						chalk.underline(implementation.name)
@@ -21,15 +31,84 @@ class ManualTesting implements Test {
 					)}: `
 				);
 
-				this.runTestCase(implementation, testCase);
+				const wasSuccessful = this.runTestCase(
+					implementation,
+					testCase
+				);
+				if (wasSuccessful) {
+					successfulRuns++;
+					successfulRunsOfCurrImplementation++;
+				}
 			}
+
+			// inform client about how many test cases passed for current
+			// implementation
+			log(
+				chalk.bold(
+					`\nRan ${chalk.underline(
+						testCases.length
+					)} test-cases for ${chalk.blue(implementation.name)}:`
+				)
+			);
+			log(
+				chalk.bold(
+					chalk.greenBright(
+						`\tTotal Passed: ${successfulRunsOfCurrImplementation}`
+					)
+				)
+			);
+			const failedTestCasesForCurrImplementation =
+				testCases.length - successfulRunsOfCurrImplementation;
+			if (failedTestCasesForCurrImplementation === 0) {
+				log(
+					chalk.bold(
+						chalk.magenta(
+							`\tTotal Failed: ${failedTestCasesForCurrImplementation}`
+						)
+					)
+				);
+			} else {
+				log(
+					chalk.bold(
+						chalk.redBright(
+							`\tTotal Failed: ${failedTestCasesForCurrImplementation}`
+						)
+					)
+				);
+			}
+		}
+
+		// inform client about stats about all test-cases
+		log(
+			chalk.bold(
+				`\n\nOverall Result on running ${chalk.underline(
+					"all test-cases"
+				)} with ${chalk.underline("all implementations")}:`
+			)
+		);
+		log(chalk.bold(chalk.greenBright(`\tTotal Passed: ${successfulRuns}`)));
+		const totalFailedTestCasesRuns = totalTestCaseRuns - successfulRuns;
+		if (totalFailedTestCasesRuns === 0) {
+			log(
+				chalk.bold(
+					chalk.magenta(`\tTotal Failed: ${totalFailedTestCasesRuns}`)
+				)
+			);
+		} else {
+			log(
+				chalk.bold(
+					chalk.redBright(
+						`\tTotal Failed: ${totalFailedTestCasesRuns}`
+					)
+				)
+			);
 		}
 	};
 
 	private runTestCase(
 		implementation: (input: any) => any,
 		testCase: TestCase<any, any>
-	) {
+	): boolean {
 		const { input, expected } = testCase;
 
 		let end = 1;
@@ -44,15 +123,20 @@ class ManualTesting implements Test {
 				chalk.underline(JSON.stringify(solReturnVal))
 			)}`;
 
-			if (_.isEqual(solReturnVal, expected))
+			if (_.isEqual(solReturnVal, expected)) {
 				log(chalk.greenBright(`\t✓ ${str}`));
-			else log(chalk.redBright(`\t✗ ${str}`));
+				return true;
+			} else {
+				log(chalk.redBright(`\t✗ ${str}`));
+				return false;
+			}
 		} catch (err) {
 			end = performance.now();
 			log(chalk.redBright(`✗ Error: ${err}`));
 		} finally {
 			log(chalk.gray(`\tRan in: ${(end - start).toPrecision(3)}ms`));
 		}
+		return false;
 	}
 }
 export default ManualTesting;
