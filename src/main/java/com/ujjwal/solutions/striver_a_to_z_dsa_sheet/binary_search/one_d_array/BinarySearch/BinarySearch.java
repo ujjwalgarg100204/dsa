d5a2ad5f3f6240f6996402d4fa7386f07a34660e
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2024-01-18 23:35:37 +0530

    Complete BinarySearch problem
    Signed-off-by: Ujjwal Garg <ujjwalgarg100204@gmail.com>

--- src/main/java/com/ujjwal/solutions/striver_a_to_z_dsa_sheet/binary_search/one_d_array/BinarySearch/BinarySearch.java
+++ src/main/java/com/ujjwal/solutions/striver_a_to_z_dsa_sheet/binary_search/one_d_array/BinarySearch/BinarySearch.java
@@ -17,8 +17,22 @@ public class BinarySearch extends DSAProblem<BinarySearchInput, Integer> {
     public List<TestCase<BinarySearchInput, Integer>> getTestCases() {
         return List.of(
                 new TestCase<>(new BinarySearchInput(new int[]{-1, 0, 3, 5, 9, 12}, 9), 4),
-                new TestCase<>(new BinarySearchInput(new int[]{-1, 0, 3, 5, 9, 12}, 2), -1)
+                new TestCase<>(new BinarySearchInput(new int[]{-1, 0, 3, 5, 9, 12}, 2), -1),
+                new TestCase<>(new BinarySearchInput(new int[]{5}, 5), 0)
         );
+    }
+
+    @Solution
+    public int iterative(int[] arr, int target) {
+        int start = 0, end = arr.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] > target) end = mid - 1;
+            else if (arr[mid] < target) start = mid + 1;
+            else return mid;
+        }
+
+        return -1;
     }
 
     @Solution
@@ -27,9 +41,9 @@ public class BinarySearch extends DSAProblem<BinarySearchInput, Integer> {
     }
 
     private int recursiveHelper(int[] arr, int target, int start, int end) {
-        if (start >= end) return -1;
+        if (end < start) return -1;
         int mid = (start + end) / 2;
-        if (arr[mid] > target) return recursiveHelper(arr, target, start, mid);
+        if (arr[mid] > target) return recursiveHelper(arr, target, start, mid - 1);
         else if (arr[mid] < target) return recursiveHelper(arr, target, mid + 1, end);
         return mid;
     }
