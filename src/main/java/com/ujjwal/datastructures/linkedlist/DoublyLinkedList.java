4a4527ad94b9702703bd4845bd7ccecc9886f425
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2024-02-08 14:06:40 +0530

    add compare method to Double Linked List
    can now compare two Double Linked List

--- src/main/java/com/ujjwal/datastructures/linkedlist/DoublyLinkedList.java
+++ src/main/java/com/ujjwal/datastructures/linkedlist/DoublyLinkedList.java
@@ -26,6 +26,24 @@ public class DoublyLinkedList {
         return head;
     }
 
+    public static <T extends Comparable<T>> int compare(DLNode<T> head1, DLNode<T> head2) {
+        DLNode<T> temp1 = head1, temp2 = head2;
+
+        while (temp1 != null && temp2 != null) {
+            if (temp1.data.compareTo(temp2.data) != 0) {
+                return temp1.data.compareTo(temp2.data);
+            }
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
+
+        if (temp1 != null || temp2 != null) {
+            return temp1 != null ? 1 : -1;
+        }
+
+        return 0;
+    }
+
     public static String toString(DLNode<?> head) {
         StringBuilder sb = new StringBuilder();
         for (DLNode<?> node = head; node != null; node = node.next) {
