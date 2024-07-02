1587db244f54a9b1ff823e0208cb6f1f384a53b9
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2024-07-02 21:46:57 +0530

    complete majority element problem

---
+++ codes/striver-a-to-z-dsa-sheet/03-arrays/medium/MajorityElement.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement {
+
+    public int brute(final int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : arr) {
+            map.merge(i, 1, Integer::sum);
+        }
+
+        int maxElement = -1;
+        int maxCount = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > maxCount) {
+                maxCount = entry.getValue();
+                maxElement = entry.getKey();
+            }
+        }
+
+        return maxElement;
+    }
+
+    public int optimal(final int[] arr) {
+        int count = 0;
+        int maxElement = -1;
+
+        for (int i : arr) {
+            if (count == 0) {
+                maxElement = i;
+                count = 1;
+            } else if (maxElement == i) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return maxElement;
+    }
+
+}
