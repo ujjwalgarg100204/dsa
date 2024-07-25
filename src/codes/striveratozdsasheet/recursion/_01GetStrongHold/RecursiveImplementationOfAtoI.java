ed7a5d68b0ed07ad8c1418b8de7f314dfb8d2575
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2024-07-25 13:39:36 +0530

    complete recursive implementation of atoi

---
+++ src/codes/striveratozdsasheet/recursion/_01GetStrongHold/RecursiveImplementationOfAtoI.java
@@ -0,0 +1,46 @@
+package codes.striveratozdsasheet.recursion._01GetStrongHold;
+
+public class RecursiveImplementationOfAtoI {
+
+  public int iterativeSolution(String s) {
+    int idx = 0;
+    // ignore all leading whitespaces
+    while (idx < s.length() && s.charAt(idx) == ' ') {
+      idx++;
+    }
+    if (idx == s.length()) {
+      // boundary check
+      return 0;
+    }
+
+    boolean isPositive = true;
+    if (s.charAt(idx) == '-' || s.charAt(idx) == '+') {
+      isPositive = s.charAt(idx) == '+';
+      idx++;
+    }
+
+    // skip leading zeroes
+    while (idx < s.length() && s.charAt(idx) == '0') {
+      idx++;
+    }
+
+    // start conversion of number
+    long number = 0;
+    while (idx < s.length() && Character.isDigit(s.charAt(idx))) {
+      number = number * 10 + Character.getNumericValue(s.charAt(idx));
+      if (number > Integer.MAX_VALUE) {
+        number = Integer.MAX_VALUE + 1L;
+      }
+      idx++;
+    }
+
+    if (isPositive) {
+      return (int) Math.min(Integer.MAX_VALUE, number);
+    } else {
+      number *= -1;
+      return (int) Math.max(Integer.MIN_VALUE, number);
+    }
+  }
+
+  // TODO: Write recursive implementation for it
+}
